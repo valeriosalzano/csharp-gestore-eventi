fd8235c82d737b75dc21ed803cd848057e3c0ca5
Valerio Salzano <vlr.slzn@gmail.com>
2023-09-18 14:10:26 +0200

    working on UpcomingEvents

---
+++ csharp-gestore-eventi/UpcomingEvents.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class UpcomingEvents
+    {
+        // PROPERTIES
+        public string Title { get; private set; }
+        public List<Event> Events { get; private set; }
+
+        // CONSTRUCTOR
+        public UpcomingEvents(string title)
+        {
+            Title = title;
+            Events = new List<Event>();
+        }
+
+        // GETTERS
+
+        public List<Event> GetEventsByDate(DateTime date)
+        {
+            List<Event> eventsFound = new List<Event>();
+
+            foreach(Event _event in Events) 
+            {
+                if(_event.Date == date )
+                    eventsFound.Add(_event);
+            }
+
+            return eventsFound;
+        }
+
+        // METHODS
+        public void AddEvent(Event newEvent)
+        {
+            this.Events.Add(newEvent);
+        }
+
+        public static string ListToString(List<Event> events)
+        {
+            string result = "";
+
+            foreach( Event _event in events)
+            {
+                result += _event.ToString()+"\n";
+            }
+
+            return result;
+        }
+
+    }
+}
