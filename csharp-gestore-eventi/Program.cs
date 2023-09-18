fd5459ba7e9465c930e4c5dd714b8da962ac725f
Valerio Salzano <vlr.slzn@gmail.com>
2023-09-18 09:40:49 +0200

    Add project files.

---
+++ csharp-gestore-eventi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_gestore_eventi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
