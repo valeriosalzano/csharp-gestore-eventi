5c70fef39232047ee0bc71128cd97b32135e65c7
Valerio Salzano <vlr.slzn@gmail.com>
2023-09-18 10:54:58 +0200

    added constructor

--- csharp-gestore-eventi/Event.cs
+++ csharp-gestore-eventi/Event.cs
@@ -11,32 +11,67 @@ namespace csharp_gestore_eventi
         // ATTRIBUTES
         private string title;
         private DateTime date;
+        private int maxSeatsCapacity;
 
         // PROPERTIES
         public string Title 
         {
             get { return this.title; }
-            set { this.title = ValidateTitle(value); }
+            set { 
+                ValidateTitle(value);
+                this.title = value;
+            }
         }
-        public DateTime Date {
+        public DateTime Date
+        {
             get { return this.date; }
-            set { this.date = ValidateDate(value); }
-        public int MaxSeatsCapacity { get; }
-        public int AvailableSeats { get; }
+            set 
+            { 
+                ValidateDate(value);
+                this.date = value;
+            }
+        }
+        public int MaxSeatsCapacity {
+            get { return this.maxSeatsCapacity; }
+            private set 
+            { 
+                ValidateMaxSeatsCapacity(value);
+                this.maxSeatsCapacity = value;
+            }
+        }
+        public int ReservedSeats { get; private set; }
 
+        // CONSTRUCTOR
+
+        public Event(string title, DateTime date, int maxSeatsCapacity)
+        {
+            this.Title = title;
+            this.Date = date;
+            this.MaxSeatsCapacity = maxSeatsCapacity;
+            this.ReservedSeats = 0;
+        }
 
         // METHODS
-        private static string ValidateTitle(string title)
+        private static void ValidateTitle(string title)
         {
             if (string.IsNullOrEmpty(title))
-                throw new Exception("This field cannot be empty or null.");
-            else
-                return title;
+                throw new ArgumentException("This field cannot be empty or null.","Title");
+
         }
 
-        private static DateTime ValidateDate(DateTime date)
+        private static void ValidateDate(DateTime date)
         {
-            return date;
+            DateTime today = DateTime.Today;
+            if (date <= today)
+                throw new ArgumentException($"Dates before {today.AddDays(1)} are not valid.", "Date");
+
+        }
+
+        private static void ValidateMaxSeatsCapacity(int seats)
+        {
+            if (seats <= 0)
+                throw new ArgumentException("This field must be greater than zero", "MaxSeatsCapacity");
+
         }
     }
 }
--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(DateTime.Today.ToString("dd/MM/yy"));
         }
     }
 }
